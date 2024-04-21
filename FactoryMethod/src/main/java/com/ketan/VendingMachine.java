7801303a5f2e7e5a5eea89bff035c4f51daf7ff3
Ketan <ketangaikwadk@gmail.com>
2024-04-21 13:23:34 +0530

    Added Factory Method Example phase 2

--- FactoryMethod/src/main/java/com/ketan/VendingMachine.java
+++ FactoryMethod/src/main/java/com/ketan/VendingMachine.java
@@ -1,17 +1,25 @@
 package com.ketan;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
 public class VendingMachine {
 
+    public static final Map<String, Supplier<Snack>> snackTypes = new HashMap<String, Supplier<Snack>>();
+
+    static {
+        snackTypes.put("ChocolateBar", ChocolateBar::new);
+        snackTypes.put("Chips", Chips::new);
+        snackTypes.put("Drink", Drink::new);
+    }
+
     //This is factory method
-    Snack getSnack(String snackType){
-        if (snackType.equals("ChocolateBar")){
-            return new ChocolateBar();
-        }else if (snackType.equals("Chips")){
-            return new Chips();
-        }else if (snackType.equals("Drink")){
-            return new Drink();
-        }else {
-            throw new IllegalArgumentException(snackType+" is not valid");
+    Snack getSnack(String snackType) {
+        if (snackTypes.containsKey(snackType) && snackTypes.get(snackType) != null) {
+            return snackTypes.get(snackType).get();
+        } else {
+            throw new IllegalArgumentException(snackType + " is not valid");
         }
     }
 }
